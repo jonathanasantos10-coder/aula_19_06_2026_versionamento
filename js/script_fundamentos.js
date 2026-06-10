6b92e7210fab28019a8333a6f2517f3e1d0b0e5f
Jonathan <jothan.a.santos10@aluno.senai.br>
2026-06-10 20:04:48 -0300

    adicionado umas variaveis no script.js para entender a logica delas

---
+++ js/script_fundamentos.js
@@ -0,0 +1,37 @@
+// comentário de uma linha
+/* 
+comentário de multiplas linhas
+*/
+
+//comando de saída
+console.log ("Hello World!")
+
+//declaracao de variaveis
+let num = 10
+var num2 = 20
+const num3 = 30
+
+console.log (num)
+console.log (num2)
+console.log (num3)
+
+if (true){
+    let num4 = 250
+    console.log (num4)
+
+    var num5 = 300
+}
+
+console.log (num5)
+
+num = 1802
+
+console.log(num)
+
+num2 = 90
+
+console.log (num2)
+
+num3 = 902
+
+console.log (num3)
